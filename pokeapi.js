b74b0414f887531bdee2f83a98b083dc34fa11fe
Jonathan <noni-06@hotmail.com>
2021-12-22 09:03:02 +0100

    pokeapi Final

--- pokeapi.js
+++ pokeapi.js
@@ -1,28 +1,37 @@
 const mainDiv$$ = document.querySelector(".container");
-/* const url$$ = `https://pokeapi.co/api/v2/pokemon/${i}`; */
+const input$$ = document.querySelector("input");
+const btnWeight$$ = document.querySelector(".weight");
+let pokemons = [];
+let filterPokemons = [];
 
 const arrayPoke = () => {
-pokemons = []
-
-for (let i = 1; i <= 151; i++) {
-  const dataBase = `https://pokeapi.co/api/v2/pokemon/${i}`;
-  pokemons.push(fetch(dataBase).then((res) => res.json()));
-}
-Promise.all(pokemons).then((results) => {
-  printPokemons(results);
-});
+  if (pokemons.length === 0) {
+    for (let i = 1; i <= 151; i++) {
+      const dataBase = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      pokemons.push(fetch(dataBase).then((res) => res.json()));
+    }
+  }
+  Promise.all(pokemons).then((results) => {
+    filterPokemons = results.filter(
+      (pokemon) =>
+        pokemon.name
+          .toLowerCase()
+          .includes(input$$.value.toLocaleLowerCase()) ||
+        pokemon.id == input$$.value
+    );
+    printPokemons(filterPokemons);
+    btnWeight$$.addEventListener("click", () => filterByWeight(pokemons));
+  });
 };
 
-
 const printPokemons = (pokemons) => {
+  mainDiv$$.innerHTML = "";
   for (const pokemon of pokemons) {
-
     let name = pokemon.name;
     let id = pokemon.id;
     let height = pokemon.height;
     let weight = pokemon.weight;
     let img = pokemon.sprites.other["official-artwork"]["front_default"];
-
 
     const divNameId$$ = document.createElement("div");
     const div$$ = document.createElement("div");
@@ -36,17 +45,17 @@ const printPokemons = (pokemons) => {
     name$$.textContent = name;
     id$$.textContent = id;
     img$$.setAttribute("src", img);
-    height$$.textContent = height/10 + " m";
-    weight$$.textContent = weight/10 + " kg";
+    height$$.textContent = height / 10 + " m";
+    weight$$.textContent = weight / 10 + " kg";
 
-    divNameId$$.classList.add('nameId')
+    divNameId$$.classList.add("nameId");
     img$$.classList.add("images");
     divStats$$.classList.add("stats");
     div$$.classList.add("card");
-    name$$.classList.add('name')
-    id$$.classList.add('id')
+    name$$.classList.add("name");
+    id$$.classList.add("id");
 
-    div$$.appendChild(divNameId$$)
+    div$$.appendChild(divNameId$$);
     divNameId$$.appendChild(name$$);
     divNameId$$.appendChild(id$$);
     div$$.appendChild(img$$);
@@ -58,4 +67,45 @@ const printPokemons = (pokemons) => {
   }
 };
 
+function filterByWeight(pokemons) {
+  let weights = [];
+  Promise.all(pokemons).then((results) => {
+    results.filter((pokemon) => {
+      pokemon.weight;
+      weights.push(pokemon.weight);
+    });
+    sortByWeight(weights);
+  });
+}
+
+const sortByWeight = (weights) => {
+  weights.sort((a, b) => b - a);
+  console.log(weights);
+
+  let finalWeights = [];
+
+  for (let i = 0; i < weights.length; i++) {
+    if (i === 0) {
+      finalWeights.push(weights[i])
+    } else {
+      if (weights[i] === weights[i-1]) {
+      } else {
+        finalWeights.push(weights[i])
+      }
+    }
+  }
+
+  let pokemonsSorted = [];
+
+  for (let i = 0; i < finalWeights.length; i++) {
+    for (let j = 0; j < filterPokemons.length; j++) {
+      if (filterPokemons[j].weight === finalWeights[i]) {
+        pokemonsSorted.push(filterPokemons[j]);
+      }
+    }
+  }
+  printPokemons(pokemonsSorted);
+};
+
 arrayPoke();
+input$$.addEventListener("input", arrayPoke);
